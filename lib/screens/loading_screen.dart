de4e0261a6fc2f38dc221c03bb3a4fd0fc8cfea4
Sanjay-2000 <sanjay.space.66@gmail.com>
2021-02-08 19:03:10 +0530

    initial commit

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,12 +11,36 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    getlocation();
+  }
+
+  void getlocation() async {
+    Location location = Location();
+    await location.getcurrentlocation();
+    print(location.longitude);
+    print(location.latitude);
+  }
+
+  void getdata() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lan=139&appid=b6907d289e10d714a6e88b30761fae22');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            //Get the current location
+            getlocation();
           },
           child: Text('Get Location'),
         ),
--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,1 +1,19 @@
+import 'package:geolocator/geolocator.dart';
 
+class Location {
+  double latitude;
+  double longitude;
+
+  Future<void> getcurrentlocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+
+      longitude = position.longitude;
+      latitude = position.latitude;
+      print(Position());
+    } catch (e) {
+      print(e);
+    }
+  }
+}
